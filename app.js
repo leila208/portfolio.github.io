f93c4926bbf6732c6399e7974f7a56f7d62da87f
leyla208 <mehdiyevaleyle@gmail.com>
2023-12-13 02:24:47 +0400

    13

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//cursor
 const cursor = document.querySelector(".cursor");
 var timeout;
 document.addEventListener("mousemove", (e) => {
@@ -15,3 +16,61 @@ document.addEventListener("mousemove", (e) => {
 document.addEventListener("mouseout", () => {
   cursor.style.display = "none";
 });
+//animatedText
+
+const words = [
+ 
+  "Web Developer. ",
+  "Frontend Developer. ",
+  
+];
+let index = 0;
+const paragraph = document.getElementById("front");
+const staticText = "Hi, I'm Leyla and I'm a ";
+const textColor = "#0d6128"; // Color for the typed words
+
+function typeWord() {
+  const word = words[index];
+  let i = 0;
+
+  function type() {
+    if (i < word.length) {
+      paragraph.innerHTML =
+        staticText +
+        `<span style="color:${textColor}">${word.substring(0, i)}</span>|`;
+      i++;
+      setTimeout(type, 100);
+    } else {
+      setTimeout(eraseWord, 2000);
+    }
+  }
+
+  type();
+}
+
+function eraseWord() {
+  let i = words[index].length;
+
+  function erase() {
+    if (i >= 0) {
+      paragraph.innerHTML =
+        staticText +
+        `<span style="color:${textColor}">${words[index].substring(
+          0,
+          i
+        )}</span>|`;
+      i--;
+      setTimeout(erase, 100);
+    } else {
+      index = (index + 1) % words.length;
+      setTimeout(typeWord, 1000);
+    }
+  }
+
+  erase();
+}
+
+typeWord();
+
+
+
